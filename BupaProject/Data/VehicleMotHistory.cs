b5b39d772e94c2219dd443917a8739bae1762ab6
zak230 <zakaria.talhi@yahoo.fr>
2024-06-19 10:46:53 +0100

    Code Refactoring

--- BupaProject/Data/VehicleMotHistory.cs
+++ BupaProject/Data/VehicleMotHistory.cs
@@ -1,36 +1,119 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace BupaProject.Data
 {
-    public class VehicleMotHistory
-    {   
-        
-        public string? Make { get; set; }
 
-        public string? Model { get; set; }
-
-        public string? primaryColour { get; set; }
-
-        public DateTime MOTExpiryDate { get; set; }
-
-        public int MileageAtLastMOT { get; set; }
-
-        public List<MotTest>? motTests { get; set; }
-
-    }
     public class MotTest
     {
-        public string? completedDate { get; set; }
-        public string? testResult { get; set; }
-        public string? expiryDate { get; set; }
-        public string? odometerValue { get; set; }
-        public string? odometerUnit { get; set; }
-        public string? motTestNumber { get; set; }
-        public List<RfrAndComment>? rfrAndComments { get; set; }
+        [JsonProperty("completedDate", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("completedDate")]
+        public string CompletedDate { get; set; }
+
+        [JsonProperty("testResult", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("testResult")]
+        public string TestResult { get; set; }
+
+        [JsonProperty("expiryDate", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("expiryDate")]
+        public string ExpiryDate { get; set; }
+
+        [JsonProperty("odometerValue", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("odometerValue")]
+        public string OdometerValue { get; set; }
+
+        [JsonProperty("odometerUnit", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("odometerUnit")]
+        public string OdometerUnit { get; set; }
+
+        [JsonProperty("motTestNumber", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("motTestNumber")]
+        public string MotTestNumber { get; set; }
+
+        [JsonProperty("rfrAndComments", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("rfrAndComments")]
+        public List<RfrAndComment> RfrAndComments { get; set; }
     }
+
     public class RfrAndComment
     {
-        public string? text { get; set; }
-        public string? type { get; set; }
+        [JsonProperty("text", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
     }
+
+    public class VehicleMotHistory
+    {
+        [JsonProperty("registration", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("registration")]
+        public string Registration { get; set; }
+
+        [JsonProperty("make", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("make")]
+        public string Make { get; set; }
+
+        [JsonProperty("model", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("model")]
+        public string Model { get; set; }
+
+        [JsonProperty("firstUsedDate", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("firstUsedDate")]
+        public string FirstUsedDate { get; set; }
+
+        [JsonProperty("fuelType", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("fuelType")]
+        public string FuelType { get; set; }
+
+        [JsonProperty("primaryColour", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("primaryColour")]
+        public string PrimaryColour { get; set; }
+
+        [JsonProperty("motTests", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonPropertyName("motTests")]
+        public List<MotTest> MotTests { get; set; } = new List<MotTest>();
+    }
+
+
+
+
+
+
+
+
+    //public class VehicleMotHistory
+    //{   
+
+    //    public string? Make { get; set; }
+
+    //    public string? Model { get; set; }
+
+    //    public string? primaryColour { get; set; }
+
+    //    public DateTime? MOTExpiryDate { get; set; }
+
+    //    public int MileageAtLastMOT { get; set; }
+
+    //    public List<MotTest>? motTests { get; set; }
+
+    //}
+    //public class MotTest
+    //{
+    //    public string? completedDate { get; set; }
+    //    public string? testResult { get; set; }
+    //    public string? expiryDate { get; set; }
+    //    public string? odometerValue { get; set; }
+    //    public string? odometerUnit { get; set; }
+    //    public string? motTestNumber { get; set; }
+    //    public List<RfrAndComment>? rfrAndComments { get; set; }
+    //}
+    //public class RfrAndComment
+    //{
+    //    public string? text { get; set; }
+    //    public string? type { get; set; }
+    //}
 }
--- BupaProject/Services/VehicleDataCheckService.cs
+++ BupaProject/Services/VehicleDataCheckService.cs
@@ -12,26 +12,28 @@ namespace BupaProject.Services
             _httpClient = httpClient;
         }
 
-        public async Task<List<VehicleMotHistory>> GetMotTestHistoryAsync(string registration)
+        public async Task<VehicleMotHistory> GetMotTestHistoryAsync(string registration)
         {
             try
             {
                 using (HttpClient client = new HttpClient())
                 {
-                    client.DefaultRequestHeaders.Add("x-api-key", "place you api key here");
+                    client.DefaultRequestHeaders.Add("x-api-key", "place apikey here");
                     var url = $"https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests?registration={registration}";
                     var response = await client.GetAsync(url);
 
                     response.EnsureSuccessStatusCode();
 
-                    return await response.Content.ReadFromJsonAsync<List<VehicleMotHistory>>();
+                    var content = await response.Content.ReadAsStringAsync();
+                    var result = await response.Content.ReadFromJsonAsync<List<VehicleMotHistory>>();
+                    return result.FirstOrDefault();
                 }
             }
             catch (Exception ex)
             {
                 Log.Error(ex, "An error occurred while fetching MOT history for registration: {Registration}", registration);
 
-                return new List<VehicleMotHistory>();
+                return null;
             }
         }
     }
