d7e637f745e3e30ab53d6bca414d85bf6688c50e
zak230 <zakaria.talhi@yahoo.fr>
2024-06-19 10:47:56 +0100

    remove comments

--- BupaProject/Data/VehicleMotHistory.cs
+++ BupaProject/Data/VehicleMotHistory.cs
@@ -78,42 +78,4 @@ namespace BupaProject.Data
         public List<MotTest> MotTests { get; set; } = new List<MotTest>();
     }
 
-
-
-
-
-
-
-
-    //public class VehicleMotHistory
-    //{   
-
-    //    public string? Make { get; set; }
-
-    //    public string? Model { get; set; }
-
-    //    public string? primaryColour { get; set; }
-
-    //    public DateTime? MOTExpiryDate { get; set; }
-
-    //    public int MileageAtLastMOT { get; set; }
-
-    //    public List<MotTest>? motTests { get; set; }
-
-    //}
-    //public class MotTest
-    //{
-    //    public string? completedDate { get; set; }
-    //    public string? testResult { get; set; }
-    //    public string? expiryDate { get; set; }
-    //    public string? odometerValue { get; set; }
-    //    public string? odometerUnit { get; set; }
-    //    public string? motTestNumber { get; set; }
-    //    public List<RfrAndComment>? rfrAndComments { get; set; }
-    //}
-    //public class RfrAndComment
-    //{
-    //    public string? text { get; set; }
-    //    public string? type { get; set; }
-    //}
 }
